1ab768eb83dbf05c8db3b8d1d9ba0df76695fb14
Excalibur783 <ismaelruiziniguez@gmail.com>
2023-03-14 19:51:05 +0100

    restar

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,15 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("¡Hola Mundo!");
         System.out.println(sumar(10,5));
+        System.out.println(restar(10,5));
     }
 
     public static int sumar(int x, int y){
         return x + y;
+    }
+
+    public static int restar(int x, int y){
+        return x - y;
     }
 }
 
