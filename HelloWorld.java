be806f190bb26393def0721b674bbb33e25e9ba5
Excalibur783 <ismaelruiziniguez@gmail.com>
2023-03-14 19:38:35 +0100

    main

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("¡Hola Mundo!");
+        System.out.println(sumar(10,5));
+    }
+
+    public static int sumar(int x, int y){
+        return x + y;
     }
 }
+
